7cc45de41923f52ac83f5630e2c1494e579807b1
jose <jose@cobbleweb.co.uk>
2020-03-18 19:00:13 -0600

    master - upload images

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 //MongoDB connection
 const connect = mongoose.connect(config.mongoUrl)
@@ -61,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
